00b8ce8402a7162a948ee77c07cc02a8b6feaaaa
chaechef <chaeefe@gmail.com>
2020-03-01 07:07:49 +0900

    feat: setup

---
+++ src/component/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const App = ({}: Props) => {
+  return <h1>Hello World!</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin({ silent: true }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.es6', '.tsx'],
+  },
+};
