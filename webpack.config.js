aad56a894cb447ddda76b12fe3a3e26b97f908f7
chaechef <chaeefe@gmail.com>
2020-03-02 22:21:07 +0900

    deps: font

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,19 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
         exclude: /node_modules/,
       },
+      {
+        // write files under 10k to inline or copy files over 10k
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
